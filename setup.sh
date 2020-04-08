9d3767dec711fa5940aedea994f9aaf0207d8627
Wilson Abesamis <wlabesamis@gmail.com>
2020-04-08 14:04:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,17 +11,7 @@ sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.c
 echo "needs_root_rights=yes" | sudo tee -a /etc/X11/Xwrapper.config
 
 #Enable tty1 tty2 tty3
-sudo systemctl enable getty@tty1
-sudo systemctl enable getty@tty2
-sudo systemctl enable getty@tty3
-
-#Enable auto login in tty1
-sudo install -b -m 644 /dev/stdin /etc/systemd/system/getty\@tty1.service.d/override.conf << EOF
-[Service]
-ExecStart=
-ExexStart=-/sbin/agetty --autologin kiosk --noclear %I $TERM
-Type=idle
-EOF
+sudo systemctl enable getty@tty1 getty@tty2 getty@tty3
 
 #Update Grub
 sudo cp grub /etc/default/grub
